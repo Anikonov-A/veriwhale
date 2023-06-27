8c87f3da65fa7f189e878660ae5eb256f18bac1c
Anton Anikonov <antonanikonov1@gmail.com>
2023-06-27 17:08:27 +0300

    Add price_slider and responsive for it

--- js/script.js
+++ js/script.js
@@ -1,7 +1,11 @@
 const btn = document.querySelector('.js-mobile-btn');
 const menu = document.querySelector('.js-mobile-menu');
 
-btn.addEventListener('click', function(){
+btn.addEventListener('click', function () {
     menu.classList.toggle('header__menu_open');
     btn.classList.toggle('header_mobile_btn_active');
 })
+
+function rangeSlide(value) {
+    document.getElementById('rangeValue').innerHTML = value;
+}
