1e464dee089391629e70c4c17b0eec2032b88611
Anton Anikonov <antonanikonov1@gmail.com>
2023-06-28 14:35:41 +0300

    add modal window

--- js/script.js
+++ js/script.js
@@ -9,3 +9,28 @@ btn.addEventListener('click', function () {
 function rangeSlide(value) {
     document.getElementById('rangeValue').innerHTML = value;
 }
+
+/*document.getElementById("open_modal_btn").addEventListener("click", function () {
+    document.getElementById("modal").classList.add("open")
+});*/
+
+let link = document.querySelector('.modal_btn');
+let modal = document.querySelector('.modal');
+document.querySelectorAll('.modal_btn').forEach(
+    link => link.addEventListener('click', event => {
+        event.preventDefault();
+        modal.classList.add('open');
+    })
+);
+window.addEventListener('keydown', (e) => {
+    if (e.key === "Escape") {
+        document.getElementById("modal").classList.remove("open")
+    }
+});
+document.querySelector("#modal .container").addEventListener('click', event => {
+    event._isClickWithInModal = true;
+});
+document.getElementById("modal").addEventListener('click', event => {
+    if (event._isClickWithInModal) return;
+    event.currentTarget.classList.remove('open');
+})
